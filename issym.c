f88dfa4f321d84c4d84b212acba26dda135bcb5a
ppetitje <ppetitje@ppetitjeU14.05>
2015-12-17 12:48:50 +0100

    +commentaire

--- issym.c
+++ issym.c
@@ -13,10 +13,12 @@ void trans(int n,int ipos, double *a,int *ja,int *ia, double *b, int *ib, int *j
             ib[j] = ib[j]+1;
          } 
       }
+    /* calcul des pointeurs à patir de leur longueur*/
     ib[0] = ipos;
     for (i=0;i<n;i++){
          ib[i+1] = ib[i] + ib[i+1];
          }
+    /*copie des éléments dans la matrice transposée*/     
     for (i=0;i<n;i++){
        for (k=ia[i];k<ia[i+1];k++){
 	    j = ja[k] ;
@@ -40,7 +42,7 @@ int issym(int nx,int n,double *a,int *ja,int *ia){
   double *atrans = malloc(sizeof(double) * (nx));
   int *iatrans = malloc(sizeof(int) * (n+1));
   int *jatrans = malloc(sizeof(int) * (nx));
-  ipos = 0;
+  ipos = 0; 
   trans(n,ipos,a,ja,ia,atrans,iatrans,jatrans);
   for (i =0;i<nx;i++){
     if (a[i] != atrans[i] || ja[i] != jatrans[i]){
